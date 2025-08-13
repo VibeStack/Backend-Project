57db2833e6f3de71477cc73e8473320e08ed8c55
Arsh Anand <arsh@Arshs-MacBook-Air.local>
2025-08-13 19:35:42 +0530

    prettier file added at right location

--- src/index.js
+++ src/index.js
@@ -3,16 +3,28 @@ import dotenv from "dotenv";
 
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants.js"
-import connectDB from "./db/index.js"
+import connectDB from "./db/index.js";
+import express from "express"
+const app = express()
 
-dotenv.config({path:'./env'})
+dotenv.config({ path: "./env" });
 
 connectDB()
-
-
-
-
-
+  .then(() => {
+    const server = app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+    server.on("error", (error) => {
+      console.log("Err:", error);
+      throw error;
+    });
+  })
+  .catch((err) => {
+    console.log(
+      "\nError as a result of Promise (DB Connection Failed) ->\n",
+      err
+    );
+  });
 
 /* 
 import express from "express"
